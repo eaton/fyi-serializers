4952bdd4cb0bb628df9fb99ac4075ff7258e9785
Jeff Eaton <jeff@eaton.fyi>
2024-04-02 18:24:48 -0500

    Pointless but fun test for PHP stringify/parse

--- test/php.test.ts
+++ test/php.test.ts
@@ -1,12 +1,20 @@
 import test from 'ava';
 import jetpack from '@eatonfyi/fs-jetpack';
-import { Toml, Php } from '../src/index.js';
+import { Php } from '../src/index.js';
+
+const data = {
+  title: "PHP Test",
+  integer: 123,
+  float: 123.4,
+  boolean: true,
+  booleanFalse: false,
+  array: [1, 2, 3, "four"],
+  object: { key: "value" }
+};
 
 test('parse sample file', t => {
-  const tomlData = jetpack.read('./test/fixtures/data.toml');
-  const phpData = jetpack.read('./test/fixtures/data.php.txt');
+  const raw = jetpack.read('./test/fixtures/data.php.txt');
+  t.is(raw, Php.stringify(data));
 
-  t.not(tomlData, undefined);
-  t.not(phpData, undefined);
-  t.deepEqual(Toml.parse(tomlData ?? ''), Php.parse(phpData ?? ''));
+  t.deepEqual(Php.parse(raw ?? ''), data);
 })
